85bf2238761b1bb56db90e71ce23d27cf4e9aea9
mcubd <106980362+mcubd@users.noreply.github.com>
2023-10-20 04:40:39 +0600

    Update Index.py

--- Index.py
+++ Index.py
@@ -1,64 +1,45 @@
-from flask import Flask, request, jsonify, send_file
 from pytube import YouTube
-from playwright.sync_api import sync_playwright
-from selenium import webdriver
-from webdriver_manager.chrome import ChromeDriverManager
-from selenium.webdriver.chrome.options import Options
+import re
+from flask import Flask, request, jsonify, send_file 
 
-#from flask import Flask, request, jsonify, send_file
-#from pytube import YouTube
-#from selenium.webdriver import Chrome
-#from playwright.sync_api import sync_playwright
-#from selenium import webdriver
-#from webdriver_manager.chrome import ChromeDriverManager
-#from selenium.webdriver.chrome.options import Options
-from selenium.webdriver.chrome.service import Service 
 app = Flask(__name__)
-service = Service(executable_path=r'/chromedriver')
-options = webdriver.ChromeOptions()
-options.add_argument('--headless')
-options.add_argument('--no-sandbox')
-
-driver = webdriver.Chrome(service=service, options=options)
-
-
-
-#chrome_options = Options()
-#chrome_options.add_argument("--headless")
-
-#browser = webdriver.Chrome(ChromeDriverManager().install(),chrome_options=chrome_options)
-
-# Sample data for demonstration
-items = []
-
-# Create an endpoint to add items
-@app.route('/items', methods=['POST'])
-def create_item():
-    data = request.get_json()
-    items.append(data)
-    return jsonify(data), 201
-
-# Create an endpoint to get all items
-@app.route('/<yt_link>', methods=['GET'])
-def read_items(yt_link):
-	driver = webdriver.Chrome()
-	return "send_file('/1.py')"
-		
-@app.route('/',methods=['POST'])
-def run():
-	data=request.get_json()
-	yt= YouTube("https://youtu.be/uRYHX4EwYYA?si=-DMcyaFRQqyloGIP")
-	return yt.streams.all()[0].url
-
-# Create an endpoint to get a specific item by ID
-@app.route('/items/<int:item_id>', methods=['GET'])
-def read_item(item_id):
-    if item_id < len(items):
-        return jsonify(items[item_id])
-    else:
-        return "Item not found", 404
-
-
-
-if __name__ == '__main__':
-    app.run(debug=False)
+@app.route('/ytt', methods=['GET'])
+def read_item():
+	return request.args.get('yt')
+@app.route('/yt', methods=['GET'])
+def read_itemm():
+	video = YouTube(request.args.get('yt'))
+	video_streams = video.streams.all()
+	title=video_streams[2].title
+	text = "This is a sample text."
+	mp4_find = "mp4"
+	res_find = 'type="video"'
+	pro_find='progressive="True"'
+	pattern1 = re.compile(f"{mp4_find}")
+	pattern2 = re.compile(f"{res_find}")
+	pattern3 = re.compile(f"{pro_find}")
+	resdict={}
+	for i in video_streams:
+		matches = pattern1.findall(i.url)
+		matches2 = pattern2.findall(str(i))
+		m3 = pattern3.findall(str(i))
+		if(matches2 and matches and m3):
+			print(i.url)
+			print(i.resolution)
+			resdict.update({i.resolution:i.url})
+			#print(resdict) print(resdict.keys())
+	#if "1080p" in resdict.keys():
+		#hd=resdict['1080p']
+		#print(resdict['1080p'])
+		#return f"{hd}&title={title}"
+	if "720p" in resdict.keys():
+		mid=resdict['720p']
+		print(resdict['720p'])
+		return f"{mid}&title={title}"
+	else:
+		print(resdict['360p'])
+		low=resdict['360p']
+		print("360 bro")
+		return f"{low}&title={title}"
+				
+if __name__ == '__main__': app.run(debug=False)
