43c89b90ee0066a0fffabd0bba3cab903552fedd
mcubd2 <148298578+mcubd2@users.noreply.github.com>
2023-12-12 20:53:41 +0600

    Update Index.py

--- Index.py
+++ Index.py
@@ -8,13 +8,13 @@ import json
 from telethon.sync import TelegramClient
 import threading
 import asyncio
-from yt_dlp import YoutubeDL
+
 
 
 accurl = 'http://prepaid.desco.org.bd/api/tkdes/customer/getBalance?accountNo=14002520&meterNo='
 account_sid = 'ACd79ad2ea41e6f1dc51c847c0bed217e5'
 auth_token = 'f3e3ed1de8917ead69a8cbafd8f7eb92'
-client = Client(account_sid, auth_token)
+
 
 
     
@@ -26,31 +26,9 @@ api_id = '28863345'
 api_hash = '0dae6aefb121ac09f5c2d07f09493452'
 phone_number = '+8801703625690'
 
-client = TelegramClient('key', api_id, api_hash)
 
 
-async def fn():
-    async with client:
-        # Find Sakib's chat
-        dialog = await client.get_input_entity('@SHADHINA') 
-        
-        msgs = await client.get_messages(dialog, limit=1)
 
-        for msg in msgs:
-            print(msg.text)
-            # bd_tz = timezone('Asia/Dhaka')
-            utc_time = msg.date
-            # bd_time = utc_time.astimezone(bd_tz)
-            print(msg.date)
-            return msg.date.timestamp()
-            # print(f"{bd_time.strftime('%I:%M%p')}")
-            # current_time = datetime.now()
-            # formatted_time = current_time.strftime("%I:%M")  # Prints time in HH:MM:SS format
-            # print("date.now:"+ formatted_time)
-
-@app.route('/tmsg')
-def tmsg():
-    return str(loop.run_until_complete(fn()))
 
 
 
@@ -87,23 +65,6 @@ def rrrr():
 	return str(res.status_code)+'-----------'+res.text
 
 
-@app.route('/yt2', methods=['GET'])
-def read_itemmn():
-    url = request.args.get('yt')
-    ydl_opts = {
-        'cookiefile': './cookies.txt'
-    }
-
-    with YoutubeDL(ydl_opts) as ydl:
-        info_dict = ydl.extract_info(url, download=False)
-        arr = info_dict['formats']
-        urls_720p = [obj.get("url") for obj in arr if obj.get("format_id") == "22"]
-        urls_360p = [obj.get("url") for obj in arr if obj.get("format_id") == "18"]
-
-        if(len(urls_720p)>0):
-            return  f"{urls_720p[0]}&title={info_dict['title']}"
-        elif(len(urls_360p)>0):
-            return  f"{urls_360p[0]}&title={info_dict['title']}"
 
 @app.route('/yt', methods=['GET'])
 def read_itemm():
